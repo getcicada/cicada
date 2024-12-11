96241e8e5c0e0131458911d32d313fe7069f93f6
Gouez <lyricsong47@gmail.com>
2024-12-12 01:30:11 +0800

    add frontend theme test

--- src/Core/Test/TestDefaults.php
+++ src/Core/Test/TestDefaults.php
@@ -10,5 +10,7 @@ use Cicada\Core\Framework\Log\Package;
 #[Package('core')]
 class TestDefaults
 {
+    final public const CHANNEL = '98432def39fc4624b33213a56b8c944d';
+
     final public const HASHED_PASSWORD = '$2y$10$XFRhv2TdOz9GItRt6ZgHl.e/HpO5Mfea6zDNXI9Q8BasBRtWbqSTS';
 }
